a9aea7f6bc41633f73a7a38b82a52ac11976c06d
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-04-14 19:33:09 -0300

    feat: get dummy posts

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,36 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('First middleware');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Acess-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
   next();
-});
+})
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: '87cyaksbc',
+      title:'First Post',
+      content: 'First post\'s content'
+    },
+    {
+      id: 'a8sduhaaa',
+      title:'Second Post',
+      content: 'Second post\'s content'
+    }
+  ];
+
+  res.status(200).json({
+    message: 'Posts fetched succesfully',
+    posts: posts
+  });
 })
 
 module.exports = app;
