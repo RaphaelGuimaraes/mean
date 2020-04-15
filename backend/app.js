6e318930dc8ea9caac686a33155ec800d2fce9c8
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-04-15 18:03:21 -0300

    chore: connect to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://raphael:oUZTMBGN8T9mulGd@cluster0-zjgga.mongodb.net/node-angular?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Database connected!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,30 +31,23 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+ post.save();
   res.status(201).json({
     message: 'Post added succesfully'
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: '87cyaksbc',
-      title:'First Post',
-      content: 'First post\'s content'
-    },
-    {
-      id: 'a8sduhaaa',
-      title:'Second Post',
-      content: 'Second post\'s content'
-    }
-  ];
-
-  res.status(200).json({
-    message: 'Posts fetched succesfully',
-    posts: posts
+  Post.find()
+  .then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched succesfully',
+      posts: documents
+    });
   });
 })
 
