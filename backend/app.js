6602993d116bf5357302ee33a338a8bfbc3287e0
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-04-20 20:42:16 -0300

    feat: update post image

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const postsRoutes = require('./routes/posts');
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://raphael:oUZTMBGN8T9mulGd@cluster0-zjgga.mongodb.net/node-angular?retryWrites=true&w=majority')
+mongoose
+  .connect(
+    "mongodb+srv://raphael:oUZTMBGN8T9mulGd@cluster0-zjgga.mongodb.net/node-angular?retryWrites=true&w=majority"
+  )
   .then(() => {
-    console.log('Database connected!');
+    console.log("Database connected!");
   })
   .catch(() => {
-    console.log('Connection failed!');
+    console.log("Connection failed!");
   })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
   );
   next();
 });
 
-app.use('/api/posts', postsRoutes);
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
