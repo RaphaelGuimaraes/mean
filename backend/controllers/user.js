82d5318e203e690f14857566275ced18b047d528
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-05-02 17:44:59 -0300

    feat: create modules

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -46,7 +46,7 @@ exports.userLogin = (req, res, next) => {
 
       const token = jwt.sign(
         { email: fetchedUser.email, userId: fetchedUser._id},
-        "this_secret_should_be_longer",
+        process.env.JWT_KEY,
         { expiresIn: "1h" });
 
       res.status(200).json({
